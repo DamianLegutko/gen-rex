62b8a500aa99e51d96a24799d2a4d1300d0351a2
Damian Legutko <domelosnet@gmail.com>
2024-03-19 09:44:41 +0100

    Update JavaDoc for base fields

--- src/main/java/pl/damianlegutko/util/genrex/base/GRexBD.java
+++ src/main/java/pl/damianlegutko/util/genrex/base/GRexBD.java
@@ -15,7 +15,7 @@ import static pl.damianlegutko.util.genrex.base.GRexBI.randomBigInteger;
  * You have to use it if {@link pl.damianlegutko.util.genrex.base.GRexD} is not enough.
  * {@link pl.damianlegutko.util.genrex.base.GRexD} can generate Double.MAX_VALUE, which is 1.7976931348623157 x 10^308.<br>
  *
- * @See That setter base on {@link pl.damianlegutko.util.genrex.base.GRexBI GenRex BigInteger field class}<br>
+ * @see pl.damianlegutko.util.genrex.base.GRexBI GenRex BigInteger field class
  */
 public class GRexBD extends GRexField<BigDecimal> {
     @JsonIgnore
@@ -36,7 +36,7 @@ public class GRexBD extends GRexField<BigDecimal> {
     /**
      * @param precision number of digits for expected BigDecimal generated value. It includes digits after decimal.
      * @Warning It's estimated precision, because number of digits is recalculated into number of bits like (2<sup>numBits</sup> - 1)<br>
-     * @See That setter base on {@link pl.damianlegutko.util.genrex.base.GRexBI GenRex BigInteger field class}<br>
+     * @see pl.damianlegutko.util.genrex.base.GRexBI GenRex BigInteger field class
      */
     @JsonIgnore
     public void setPrecision(int precision) {
--- src/main/java/pl/damianlegutko/util/genrex/base/GRexL.java
+++ src/main/java/pl/damianlegutko/util/genrex/base/GRexL.java
@@ -2,6 +2,9 @@ package pl.damianlegutko.util.genrex.base;
 
 import java.util.function.Supplier;
 
+/**
+ * That {@link pl.damianlegutko.util.genrex.base.GRexField Field} can generate value for scope from 0 to {@link java.lang.Long#MAX_VALUE}, which is 9,223,372,036,854,775,807 (2<sup>63</sup> - 1).
+ */
 public class GRexL extends GRexSupplier<Long> {
     public GRexL(Supplier<Long> generationMethod) {
         super(generationMethod);
